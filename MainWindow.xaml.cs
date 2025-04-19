6dbed5ed91bfcb6b08986139276476f802001cfc
Juan Gomez <juangomez130703@gmail.com>
2025-04-18 21:37:27 -0500

    Lambda

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,5 +20,44 @@ namespace WPFApp
         {
             InitializeComponent();
         }
-    }
+
+        void CreateTask()
+        {
+            Task T;
+            var Code = new Action(ShowMesage);
+            T = new Task(Code);
+            Task T2 = new Task(delegate
+            {
+                MessageBox.Show("Ejecutando una tarea en un método anónimo.");
+            }
+            );
+
+            Task T3A = new Task(ShowMesage);
+            Task T3 = new Task(
+                () => ShowMesage());
+
+            Task T4 = new Task(() => MessageBox.Show("Ejecutando la tarea 4"));
+
+            Task T5 = new Task(() =>
+                {
+                    DateTime CurrentDate = DateTime.Today;
+                    DateTime StarDate = CurrentDate.AddDays(30);
+                    MessageBox.Show($"Tarea 5. Fecha Calculada: {StarDate}");
+                }   
+            );
+
+            Task T6 = new Task((message) =>
+            MessageBox.Show(message), "Expresión Lambda con parámetros.");
+        }
+
+          
+
+
+        void ShowMesage()
+        {
+            MessageBox.Show("Ejecutando el método ShowMessage");
+        }
+
+        
+}
 }
